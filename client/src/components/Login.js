f620aca094cab31e49518d705cba55845842fa49
shubh-2012 <shubh-2012@users.noreply.github.com>
2021-07-26 13:13:22 +0530

    UI for login and signup

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,45 @@
 import React from 'react'
-
+import picture1 from '../images/picture1.jpg';
+import '../App.css';
 const Login=()=>{
     return (
-        <div>
-            <p>Welcome</p>
-            <h1>Login</h1>
-        </div>
+        <>
+            <section id="home">
+  			<div className="row">
+  				
+  			
+	  			<div className="column">
+	  				<img className="home-image" src={picture1} alt="picture1" />
+	  			</div>
+
+	  			<div className="column">
+	  				<div className="row">
+				    <div className="col-md-6 mx-auto p-0">
+				        <div className="card">
+				            <div className="login-box">
+				                <div className="login-snip" > <label for="tab" className="tab">Sign Up</label>
+				                    <div className="login-space">
+				                        
+                                    <div className="login">
+				                            <div className="group"> <label for="user" className="label">Username</label> <input id="user" type="text" className="input" placeholder="Enter your username"/> </div>
+				                            <div className="group"> <label for="pass" className="label">Password</label> <input id="pass" type="password" className="input" data-type="password" placeholder="Enter your password"/> </div>
+				                            <div className="group"> <input id="check" type="checkbox" className="check" checked/> <label style={{color:"black"}} for="check"><span className="icon"></span> Keep me Signed in</label> </div>
+				                            <div className="group"> <input type="submit" className="button" value="Sign In"/> </div>
+				                            <div className="hr"></div>
+				                            <div className="foot"> <a href="#">Forgot Password?</a> </div>
+				                        </div>
+				                    </div>
+				                </div>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+
+  			</div>
+  				
+			</div> 
+  		</section>
+        </>
     )
 }
 
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,11 +1,48 @@
 import React from 'react'
-
-const Signup=()=>{
+import picture1 from '../images/picture1.jpg';
+import '../App.css';
+const Signup = () =>{
     return (
-        <div>
-            <p>Welcome</p>
-            <h1>Signup</h1>
-        </div>
+        <>
+        
+            <section id="home">
+  			<div className="row">
+  				
+  			
+	  			<div className="column">
+	  				<img className="home-image" src={picture1} alt="picture1" />
+	  			</div>
+
+	  			<div className="column">
+	  				<div className="row">
+				    <div className="col-md-6 mx-auto p-0">
+				        <div className="card">
+				            <div className="login-box">
+				                <div className="login-snip" > <label for="tab" className="tab">Sign Up</label>
+				                    <div className="login-space">
+				                        
+				                        <div className="sign-up-form">
+				                            <div className="group"> <label for="user" className="label">Username</label> <input id="user" type="text" className="input" placeholder="Create your Username"/> </div>
+				                            <div className="group"> <label for="pass" className="label">Password</label> <input id="pass" type="password" className="input" data-type="password" placeholder="Create your password"/> </div>
+				                            <div className="group"> <label for="pass" className="label">Repeat Password</label> <input id="pass" type="password" className="input" data-type="password" placeholder="Repeat your password"/> </div>
+				                            <div className="group"> <label for="pass" className="label">Email Address</label> <input id="pass" type="text" className="input" placeholder="Enter your email address"/> </div>
+				                            <div className="group"> <input type="submit" className="button" value="Sign Up"/> </div>
+				                            <div className="hr"></div>
+				                            <div className="foot"> <label for="tab-1">Already a Member?</label> </div>
+				                        </div>
+				                    </div>
+				                </div>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+
+  			</div>
+  				
+			</div> 
+  		</section>
+        
+        </>
     )
 }
 
