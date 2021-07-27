f086acdacf71801048d0564eb77f5eb047e2c47a
shubh-2012 <shubh-2012@users.noreply.github.com>
2021-07-27 17:21:05 +0530

    new registration and signup authentication through UI

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React,{useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory} from 'react-router-dom';
 import picture1 from '../images/picture1.jpg';
 import '../App.css';
 const Signup = () =>{
+
+	const history= useHistory();
+	const[user,setUser]=useState({
+		name:"",email:"",phone:"",work:"",password:"",cpassword:""
+	});
+	let name,value;
+	const handleInputs = (e) => {
+		console.log(e);
+		name=e.target.name;
+		value=e.target.value;
+
+		setUser({...user,[name]:value});
+	}
+
+	const PostData = async (e)=>{
+		e.preventDefault();
+
+		const {name,email,phone,work,password,cpassword}= user;
+
+		const res= await fetch("/register",{
+			method:"POST",
+			headers:{
+				"Content-Type":"application/json"
+			},
+			body:JSON.stringify({
+				name,email,phone,work,password,cpassword
+			})
+		});
+
+		const data= await res.json();
+		if(data.status=== 422 || !data){
+			window.alert("Invlaid");
+			console.log("Invalid Registration");
+		}
+		else{
+			window.alert("Successfull");
+			console.log("successfull registration");
+
+			history.push("/login");
+		}
+	}
+
     return (
         <>
         
@@ -23,15 +65,36 @@ const Signup = () =>{
 				                <div className="login-snip" > <label for="tab" className="tab">Sign Up</label>
 				                    <div className="login-space">
 				                        
-				                        <div className="sign-up-form">
-				                            <div className="group"> <label for="user" className="label">Username</label> <input id="user" type="text" className="input" placeholder="Create your Username"/> </div>
-				                            <div className="group"> <label for="pass" className="label">Password</label> <input id="pass" type="password" className="input" data-type="password" placeholder="Create your password"/> </div>
-				                            <div className="group"> <label for="pass" className="label">Repeat Password</label> <input id="pass" type="password" className="input" data-type="password" placeholder="Repeat your password"/> </div>
-				                            <div className="group"> <label for="pass" className="label">Email Address</label> <input id="pass" type="text" className="input" placeholder="Enter your email address"/> </div>
-				                            <div className="group"> <input type="submit" className="button" value="Sign Up"/> </div>
+				                        <form method="POST" className="sign-up-form" id="register-form">
+				                            <div className="group"> <label for="user" className="label">Username</label> <input name="name" id="user" type="text" className="input"
+											value={user.name}
+											onChange={handleInputs}
+											 placeholder="Create your Username"/> </div>
+											<div className="group"> <label for="user" className="label">Email</label> <input name="email" id="user" type="text" className="input"
+											value={user.email}
+											onChange={handleInputs}
+											 placeholder="Create your Username"/> </div>
+											 <div className="group"> <label for="user" className="label">Phone</label> <input name="phone" id="user" type="text" className="input"
+											value={user.phone}
+											onChange={handleInputs}
+											 placeholder="Create your Username"/> </div>
+											<div className="group"> <label for="pass" className="label">Work</label> <input name="work" id="pass" type="text" className="input"
+											value={user.work}
+											onChange={handleInputs}
+											 placeholder="Enter your email address"/> </div>
+				                            <div className="group"> <label for="pass" className="label">Password</label> <input name="password" id="pass" type="password" className="input" data-type="password"
+											value={user.password}
+											onChange={handleInputs}
+											 placeholder="Create your password"/> </div>
+				                            <div className="group"> <label for="pass" className="label">Repeat Password</label> <input name="cpassword" id="pass" type="password" className="input" data-type="password"
+											value={user.cpassword}
+											onChange={handleInputs}
+											 placeholder="Repeat your password"/> </div>
+				                            
+				                            <div className="group"> <input type="submit" name="signup" className="button" value="register"  onClick={PostData}/></div>
 				                            <div className="hr"></div>
 				                            <div className="foot"> <NavLink to="Login">Already Registered?</NavLink> </div>
-				                        </div>
+				                        </form>
 				                    </div>
 				                </div>
 				            </div>
